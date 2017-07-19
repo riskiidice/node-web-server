5868508a1ebc493884a5058c5af5dd5a49de7ca3
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-07-19 22:32:45 +0700

    test routing

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    // res.send('Hello Express');
+    res.send({
+        name: 'Tikumporn',
+        likes: ['Dogs', 'cats']
+    })
 });
 
-app.listen(3000);
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage : 'Unable to handle request'
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
