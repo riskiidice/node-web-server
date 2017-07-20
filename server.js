be5208082bf88a2f5c566ac35f7ac3ac4154e0ec
tikumporn wankwa <505144@pea.co.th>
2017-07-20 13:00:58 +0700

    Adding Middleware

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}  ${req.url} ${req.ip}`;
+
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n');
+    next();
+});
+
+/* app.use((req, res, next)=> {
+    res.render('maintenance.hbs');
+});
+ */
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     })
 });
 
