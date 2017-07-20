02fb327f161ea72d02bc770a4b134b0c97d3b832
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-07-20 11:01:20 +0700

    Add hbs

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    // res.send('Hello Express');
-    res.send({
-        name: 'Tikumporn',
-        likes: ['Dogs', 'cats']
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 });
 
 app.get('/bad', (req, res) => {
     res.send({
-        errorMessage : 'Unable to handle request'
+        errorMessage: 'Unable to handle request'
     });
 });
 
