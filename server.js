a4bd8f994f5c5e88d067c6f724048fc28a36f5c1
tikumporn wankwa <505144@pea.co.th>
2017-07-21 17:13:05 +0700

    Adding Spark bot

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const axios = require('axios');
 
 const port = process.env.PORT || 3000;
+
 var app = express();
+var bodyParser = require('body-parser');
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -16,6 +19,9 @@ app.use((req, res, next) => {
     fs.appendFileSync('server.log', log + '\n');
     next();
 });
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 /* app.use((req, res, next)=> {
     res.render('maintenance.hbs');
@@ -50,7 +56,36 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.post('/webhook', (req, res) => {
+    var body = req.body;
+
+
+     var instance = axios.create({
+        baseURL: "",
+        headers: { 'Authorization': 'Bearer NDVkNDUxMGUtNDc3ZC00YTM1LWEzMWYtZmNkMjkwMjU3ZDUyYzU5ZTcwNjMtZGVl' }
+    });
+
+    var sparkKeys = {
+        roomId: "Y2lzY29zcGFyazovL3VzL1JPT00vZTM3YzE1NjAtNmRjZS0xMWU3LWI2OWItMDk4ODQzNWEzOGRk",
+        text: body.text
+    };
+
+    axios({
+        method : 'post',
+        url: 'https://api.ciscospark.com/v1/messages',
+        headers: { 'Authorization': 'Bearer NDVkNDUxMGUtNDc3ZC00YTM1LWEzMWYtZmNkMjkwMjU3ZDUyYzU5ZTcwNjMtZGVl' },
+        data : sparkKeys
+    })
+        .then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        }); 
+ 
+
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
